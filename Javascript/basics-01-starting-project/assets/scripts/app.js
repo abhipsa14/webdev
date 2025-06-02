04e2c5c52327904cb8555d5ec32160b2be0efeb6
abhipsa14 <abhipsasri8183@gmail.com>
2025-06-02 21:50:12 +0530

    file-added

---
+++ Javascript/basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,7 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+currentResult = currentResult+10;
+ 
+let calculationDescription = `Hi your result`;
+let errorMessage = `Please enter \na valid number!`;
+outputResult(currentResult,errorMessage);
